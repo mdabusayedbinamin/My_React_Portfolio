9b087783817bed0788057ad67208d306e200a1ea
abu sayed <mdabusayedbinamin@gmail.com>
2026-01-18 01:39:07 +0600

    update feture

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
         </div>
         <div className="hero-resume" data-aos="fade-left" data-aos-delay="900">
           <a
-            href="MD-ABU-SAYED.pdf"
+            href="MD_ABU_SAYED Resume.pdf"
             target="_blank"
             download="MD_ABU_SAYED_Resume.pdf"
           >
